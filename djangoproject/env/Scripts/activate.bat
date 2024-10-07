b40df6570db736cd96d6ee88b422aaabd7f313d4
Zingleh <a.olguinparraq@gmail.com>
2024-10-07 20:26:13 -0300

    arreglando

--- djangoproject/env/Scripts/activate.bat
+++ djangoproject/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\basti\Portafolio_Titulo\djangoproject\env
+set VIRTUAL_ENV=C:\Users\Darla\Desktop\PORTAFOLIO\Portafolio_Titulo\djangoproject\env
 
 if not defined PROMPT set PROMPT=$P$G
 
