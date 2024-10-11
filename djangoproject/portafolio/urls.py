669df10947c666108efd756aae3e0d258f71b6d1
bastian <ba.madrid@duocuc.cl>
2024-10-11 16:56:25 -0300

    Validaciones contraseña y diseño

---
+++ djangoproject/portafolio/urls.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+from django.urls import path
+from ecommerse.views import agregar_producto, eliminar_producto, limpiar_carrito, restar_producto, home, carrito, buscar, modal, guardar_carrito
+from ecommerse import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('buscar/', buscar, name='buscar'),
+    path('', home, name='home'),
+    path('modal/', modal, name='modal'),
+    path('carrito/', carrito, name='carrito'),
+    path('guardar_carrito/', guardar_carrito, name='guardar_carrito'),
+    #Paths de Carrito de compras
+    path('agregar/<int:producto_id>/', agregar_producto, name="Add"),
+    path('eliminar/<int:producto_id>/', eliminar_producto, name="Del"),
+    path('restar/<int:producto_id>/', restar_producto, name="Sub"),
+    path('limpiar/', limpiar_carrito, name="CLS"),
+    #Paths de Sistema Login
+    path('registro/', views.registro, name='registro'),
+    path('inicio_sesion/', views.inicio_sesion, name='inicio_sesion'),
+    path('cerrar_sesion/', views.cerrar_sesion, name='cerrar_sesion'),
+    path('read_sesion/', views.read_ingreso , name='read_ingreso'),
+    #Paths para el recuperar contraseña
+    # URL para la vista de recuperación de contraseña
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form_custom.html'), name='password_reset'),
+    # URL que se mostrará después de enviar el formulario de recuperación
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done_custom.html'), name='password_reset_done'),
+    # URL para la vista que procesa el enlace enviado por correo
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm_custom.html'), name='password_reset_confirm'),
+    # URL para la vista cuando la contraseña ha sido reseteada con éxito
+    path('reset_done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete_custom.html'), name='password_reset_complete'),
+
+]
+
