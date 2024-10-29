f1dd82c384fbd0ea42821116df0114be7a084632
Manolo27jaja <man.diaz@duocuc.cl>
2024-10-29 17:27:54 -0300

    arreglando conflictos % otros

--- Fase 2/Evidencias del proyecto/Evidencias del sistema/djangoproject/ecommerse/models.py
+++ Fase 2/Evidencias del proyecto/Evidencias del sistema/djangoproject/ecommerse/models.py
@@ -19,7 +19,6 @@ class Producto(models.Model):
 #________________________________________________________        
 
 #___________________________________
-<<<<<<< HEAD
 class UsuarioManager(BaseUserManager): # Administracion personalizado para el modelo usuario, permite crear usuarios y superusuarios de manera mas sensilla
     def create_user(self, email, nombre, password, telefono): # El metodo crea un nuevo usuario con los 4 campos obligatorios
         if not email:
@@ -36,8 +35,8 @@ class UsuarioManager(BaseUserManager): # Administracion personalizado para el mo
         user.save(using=self._db) # Guarda el usuario en la base de datos
         return user
 
-    def create_superuser(self, email, nombre, password): # Metodo para crear el super usuario, con los campos especificados
-        user = self.create_user(email, nombre, password) # Se pasan los argumentos al create_user, devolviendo un objeto de tipo usuario 
+    def create_superuser(self, email, nombre, password, telefono): # Metodo para crear el super usuario, con los campos especificados
+        user = self.create_user(email, nombre, password, telefono) # Se pasan los argumentos al create_user, devolviendo un objeto de tipo usuario 
         user.is_staff = True
         user.is_superuser = True # Se le asignan las propiedades de un super usuario
         user.save(using=self._db) # Lo guarda en la base de datos
@@ -46,7 +45,7 @@ class UsuarioManager(BaseUserManager): # Administracion personalizado para el mo
 class Usuario(AbstractBaseUser, PermissionsMixin): # Definimos un modelo personalizado para nuesto usuario/ AbstractBaseUser nos proporciona la insfraestructura basica para un usuario personalizado/Añade soperte para el sistema de permisos de django
     email = models.EmailField(unique=True) # Correo unico
     nombre = models.CharField(max_length=30) 
-    telefono = models.CharField(max_length=15, blank=False, null=False) # El telefono puede ser nullo o blank
+    telefono = models.CharField(max_length=15, blank=False, null=True) # El telefono puede ser nullo o blank
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False) # por default es false a menos que se cree como super usuario por medio de comandos
     is_superuser = models.BooleanField(default=False) # por default es false a menos que se cree como super usuario por medio de comandos
@@ -55,49 +54,14 @@ class Usuario(AbstractBaseUser, PermissionsMixin): # Definimos un modelo persona
 
     USERNAME_FIELD = 'email' # Se define el email como identificador principal
     REQUIRED_FIELDS = ['nombre','telefono'] # Se define que los campos adicionales son obligatorios (para el super usuario)
-=======
-class UsuarioManager(BaseUserManager):
-    def create_user(self, email, nombre, password=None, telefono=None):
-        if not email:
-            raise ValueError('El email es obligatorio')
-        email = self.normalize_email(email)
-        user = self.model(email=email, nombre=nombre, telefono=telefono)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
 
-    def create_superuser(self, email, nombre, password=None):
-        user = self.create_user(email, nombre, password)
-        user.is_staff = True
-        user.is_superuser = True
-        user.save(using=self._db) 
-        return user
-
-class Usuario(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(unique=True, default='example@example.com')
-    nombre = models.CharField(max_length=30, default='Usuario')
-    telefono = models.CharField(max_length=15, blank=True, null=True)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
-
-    objects = UsuarioManager()
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['nombre']
->>>>>>> 02e8b3697dcc47d645419c7bd46c834e358461b4
-
-    def _str_(self):
+    def __str__(self):
         return self.email
 
 
     def has_perm(self, perm, obj=None):
         """Verifica si el usuario tiene un permiso específico."""
-<<<<<<< HEAD
         return self.is_superuser  # verifica si el usuario tiene permisos
-=======
-        return self.is_superuser  # Los superusuarios tienen todos los permisos
->>>>>>> 02e8b3697dcc47d645419c7bd46c834e358461b4
 
     def has_module_perms(self, app_label):
         """Verifica si el usuario tiene permisos para ver una app específica."""
--- Fase 2/Evidencias del proyecto/Evidencias del sistema/djangoproject/portafolio/urls.py
+++ Fase 2/Evidencias del proyecto/Evidencias del sistema/djangoproject/portafolio/urls.py
@@ -1,20 +1,17 @@
 from django.contrib import admin
 from django.urls import path
-from ecommerse.views import agregar_producto, eliminar_producto, limpiar_carrito, restar_producto, home, buscar, modal, guardar_carrito, carritoid
+from ecommerse.views import detalle_producto, agregar_producto, eliminar_producto, limpiar_carrito, restar_producto, home, buscar, modal, guardar_carrito, carritoid
 from ecommerse import views
 from django.contrib.auth import views as auth_views
-<<<<<<< HEAD
 from django.conf import settings
 from django.conf.urls.static import static
 
-=======
->>>>>>> 02e8b3697dcc47d645419c7bd46c834e358461b4
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('buscar/', buscar, name='buscar'),
     path('', home, name='home'),
-    path('modal/', modal, name='modal'),
+    path('detalle_producto/', detalle_producto, name='detalle_producto'),
     path('carritoid/<int:producto_id>/', carritoid, name='carritoid'),
     path('guardar_carrito/', guardar_carrito, name='guardar_carrito'),
     #Paths de Carrito de compras
@@ -22,17 +19,13 @@ urlpatterns = [
     path('eliminar/<int:producto_id>/', eliminar_producto, name="Del"),
     path('restar/<int:producto_id>/', restar_producto, name="Sub"),
     path('limpiar/', limpiar_carrito, name="CLS"),
-<<<<<<< HEAD
     path('miCarrito/', views.miCarrito, name="miCarrito"),
     path('miCarrito/', views.mostrar_carrito, name='miCarrito'),
-=======
->>>>>>> 02e8b3697dcc47d645419c7bd46c834e358461b4
     #Paths de Sistema Login
     path('registro/', views.registro, name='registro'),
     path('inicio_sesion/', views.inicio_sesion, name='inicio_sesion'),
     path('cerrar_sesion/', views.cerrar_sesion, name='cerrar_sesion'),
     path('read_sesion/', views.read_ingreso , name='read_ingreso'),
-<<<<<<< HEAD
     #Paths para el recuperar contraseña
     # URL para la vista de recuperación de contraseña
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form_custom.html'), name='password_reset'),
@@ -47,18 +40,3 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-=======
-    path('recuperar/', views.recuperar, name='recuperar'),
-    path('registro/', views.registro, name='registro'),
-    #Paths para el recuperar contraseña
-    # URL para la vista de recuperación de contraseña
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    # URL que se mostrará después de enviar el formulario de recuperación
-    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    # URL para la vista que procesa el enlace enviado por correo
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    # URL para la vista cuando la contraseña ha sido reseteada con éxito
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-]
-
->>>>>>> 02e8b3697dcc47d645419c7bd46c834e358461b4
